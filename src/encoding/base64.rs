f50e6c44a7f2ec3478dd50c47b8a7d1591ba0abf
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-18 02:36:49 -0400

    base64 encoding cleanup

--- src/encoding/base64.rs
+++ src/encoding/base64.rs
@@ -1,6 +1,5 @@
 use crate::encoding::Encode;
 
-/// https://en.wikipedia.org/wiki/Base64#Base64_table
 const CHAR_TABLE: [char; 64] = [
     'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
     'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l',
@@ -10,34 +9,32 @@ const CHAR_TABLE: [char; 64] = [
 
 const PADDING: char = '=';
 
+/// https://tools.ietf.org/html/rfc4648
 pub struct Base64;
 
 impl Encode for Base64 {
     fn encode(input: &[u8]) -> String {
-        let mut output: String = String::new();
         let len = input.len();
-        let chunk_count = len / 3;
-        let last_len = len % 3;
+        let remainder = len % 3;
 
-        for i in 0..chunk_count {
-            encode(
-                (input[i * 3] as u32) << 16
-                    | (input[i * 3 + 1] as u32) << 8
-                    | input[i * 3 + 2] as u32,
-                &mut output,
-            );
+        let mut output: String = String::with_capacity(len / 3 * 4 + 3);
+
+        for i in (0..len - remainder).step_by(3) {
+            println!("{}", i);
+            encode_part(&input[i..i + 3], &mut output);
         }
 
-        // trash dumpster
-        if last_len == 1 {
-            output.push(CHAR_TABLE[(input[len - 1] >> 2) as usize]);
-            output.push(CHAR_TABLE[((input[len - 1] & 0b11) << 4) as usize]);
-            output.push(PADDING);
-            output.push(PADDING);
-        } else if last_len == 2 {
-            output.push(CHAR_TABLE[(input[len - 2] >> 2) as usize]);
-            output.push(CHAR_TABLE[((input[len - 2] & 0b11) << 4 | input[len - 1] >> 4) as usize]);
-            output.push(CHAR_TABLE[((input[len - 1] & 0b1111) << 2) as usize]);
+        if remainder != 0 {
+            if remainder == 1 {
+                output.push(CHAR_TABLE[first_subpart(input[len - 1]) as usize]);
+                output.push(CHAR_TABLE[second_subpart(input[len - 1], 0) as usize]);
+                output.push(PADDING);
+            } else if remainder == 2 {
+                output.push(CHAR_TABLE[first_subpart(input[len - 2]) as usize]);
+                output.push(CHAR_TABLE[second_subpart(input[len - 2], input[len - 1]) as usize]);
+                output.push(CHAR_TABLE[third_subpart(input[len - 1], 0) as usize]);
+            }
+
             output.push(PADDING);
         }
 
@@ -45,11 +42,31 @@ impl Encode for Base64 {
     }
 }
 
-fn encode(input: u32, output: &mut String) {
-    output.push(CHAR_TABLE[(input >> 18 & 0b111111) as usize]);
-    output.push(CHAR_TABLE[(input >> 12 & 0b111111) as usize]);
-    output.push(CHAR_TABLE[(input >> 6 & 0b111111) as usize]);
-    output.push(CHAR_TABLE[(input & 0b111111) as usize]);
+fn encode_part(input: &[u8], output: &mut String) {
+    output.push(CHAR_TABLE[first_subpart(input[0]) as usize]);
+    output.push(CHAR_TABLE[second_subpart(input[0], input[1]) as usize]);
+    output.push(CHAR_TABLE[third_subpart(input[1], input[2]) as usize]);
+    output.push(CHAR_TABLE[fourth_subpart(input[2]) as usize]);
+}
+
+/// 11111100_00000000_00000000
+fn first_subpart(a: u8) -> u8 {
+    a >> 2
+}
+
+/// 00000011_11110000_00000000
+fn second_subpart(a: u8, b: u8) -> u8 {
+    (a & 0b11) << 4 | b >> 4
+}
+
+/// 00000000_00001111_11000000
+fn third_subpart(b: u8, c: u8) -> u8 {
+    (b & 0b1111) << 2 | c >> 6
+}
+
+/// 00000000_00000000_00111111
+fn fourth_subpart(c: u8) -> u8 {
+    c & 0b111111
 }
 
 #[cfg(test)]
