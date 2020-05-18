e7cfe80552419ae2ba039b677c86bf683f0ab8ed
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-18 00:57:55 -0400

    match nibble cleanup

--- src/encoding/mod.rs
+++ src/encoding/mod.rs
@@ -21,22 +21,9 @@ pub fn unhex(input: &[u8]) -> Vec<u8> {
 
 fn match_nibble(input: u8) -> u8 {
     match input {
-        0x30 => 0,
-        0x31 => 1,
-        0x32 => 2,
-        0x33 => 3,
-        0x34 => 4,
-        0x35 => 5,
-        0x36 => 6,
-        0x37 => 7,
-        0x38 => 8,
-        0x39 => 9,
-        0x41 | 0x61 => 10,
-        0x42 | 0x62 => 11,
-        0x43 | 0x63 => 12,
-        0x44 | 0x64 => 13,
-        0x45 | 0x65 => 14,
-        0x46 | 0x66 => 15,
+        0x30..=0x39 => input - 0x30,
+        0x41..=0x46 => input - 0x37,
+        0x61..=0x66 => input - 0x57,
         _ => panic!("your hex is broke mate"),
     }
 }
