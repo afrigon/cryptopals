bdb03a6818ff0dafcb8f307ca43cedc80eed2894
Alexandre Frigon <alexandre.frigon.1@gmail.com>
2020-05-20 02:30:20 -0400

    repeating xor encryption

--- src/cryptopals.rs
+++ src/cryptopals.rs
@@ -73,4 +73,17 @@ mod set1 {
 
         assert_eq!(message, expected);
     }
+
+    #[test]
+    fn challenge5() {
+        let key = b"ICE";
+        let message =
+            b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+        let expected =
+            unhex(b"0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f");
+
+        let cipher = xor(message, key);
+
+        assert_eq!(cipher, expected);
+    }
 }
